edcc84b9e9123c817a7609b5cd9e6a41591d64db
asahni <asahni@snapchat.com>
2024-11-06 14:27:55 -0800

    simplify test case, remove nit

--- torchao/prototype/low_bit_optim/adam.py
+++ torchao/prototype/low_bit_optim/adam.py
@@ -13,7 +13,7 @@ from .quant_utils import _fp32_to_bf16_sr
 
 class _AdamBase(Optimizer):
     def __init__(
-        self, params, lr, betas, eps, weight_decay, amsgrad, *, block_size, bf16_stochastic_round, is_adamw,exclude_low_bit_optim_params=None
+        self, params, lr, betas, eps, weight_decay, amsgrad, *, block_size, bf16_stochastic_round, is_adamw, exclude_low_bit_optim_params=None
     ) -> None:
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
